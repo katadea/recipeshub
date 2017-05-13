022ca4c71d7c73af2df8d7a47bbbf57ea75ea1bf
Kathryn Adea <Kathryn Adea>
2017-05-13 03:57:48 -0700

    2nd

---
+++ js/index.js
@@ -0,0 +1,62 @@
+var jsonURL = 'https://s3-us-west-2.amazonaws.com/digicode-interview/Q1.json';
+
+Vue.config.devtools = false;
+Vue.config.productionTip = false;
+
+var app = new Vue({
+
+  el: '#app',
+
+  data: {
+    recipes: [],
+    IngredientsDetails: {},
+    error: false
+    },
+
+  /* created: function () {
+    this.fetchData();
+  }, */
+
+	mounted: function() {
+			//console.log ("Readying");
+        this.$http({ url: jsonURL, method: 'GET' }).then(function(response) {
+            this.$set(this, 'recipes', response.data.recipes);
+            this.$set(this, 'IngredientsDetails', response.data.IngredientsDetails);
+            //console.log("Recipes and ingredients data loaded");
+        }, function(response) {
+            this.$set(this, 'error', true);
+            console.log("HTTP Request Failed")
+        });
+    },
+
+   methods: {
+   	getValue: function (key, object) {
+   		return object[key];
+   	}
+   } 
+  
+}); 
+
+$(document).ready(function () {
+	
+	//Toggle recipe cards
+	$('.recipe-item, .cuisineLink').click( function () {
+		//console.log('Clicked link');
+		var name = $.trim($(this).text());
+		//console.log(name);
+		toggleCard(name);
+	});
+	
+	//Initialize popovers
+	$(function () {
+  		$('[data-toggle="popover"]').popover()
+	});
+	
+})
+
+var toggleCard = function (id) {
+	$('#rightCol .panel').css('display','none');
+	//console.log('The id is #'+id);
+	$('#'+id).css('display','block');
+	//console.log('element displayed');
+};
